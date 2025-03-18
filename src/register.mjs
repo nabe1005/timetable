25b9ed62b7cd04936441803b0a3f4dfb54922c18
nabe1005 <36324839+nabe1005@users.noreply.github.com>
2025-03-18 13:47:15 +0900

    ✨ indexedDBの値を参照して一覧表示できるようにした

--- src/register.mjs
+++ src/register.mjs
@@ -1,7 +1,12 @@
+import { ClassListItemComponent } from "./component/class-list-item.mjs";
 import { ClassEditPage } from "./pages/class-edit.mjs";
 import { ClassListPage } from "./pages/class-list.mjs";
 import { HomePage } from "./pages/home.mjs";
 
+// Pages
 customElements.define("home-page", HomePage);
 customElements.define("class-list-page", ClassListPage);
 customElements.define("class-edit-page", ClassEditPage);
+
+// Components
+customElements.define("class-list-item", ClassListItemComponent);
