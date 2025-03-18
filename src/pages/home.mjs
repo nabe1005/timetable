5893172ec4a8a5d86e12b4a9f4818a93adeef5e7
nabe1005 <36324839+nabe1005@users.noreply.github.com>
2025-03-18 14:07:37 +0900

    ✨ ホーム画面で時間割の雛形を表示できるようにした

--- src/pages/home.mjs
+++ src/pages/home.mjs
@@ -1,8 +1,50 @@
+import { basicStyle } from "../shared/style.mjs";
+
 export class HomePage extends HTMLElement {
   /** @type {ShadowRoot | undefined} */
   shadowRoot = undefined;
 
-  html = () => /* HTML */ ` <span>home page</span> `;
+  css = () => /* CSS */ `
+  ${basicStyle}
+
+  :host .home {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: 32px repeat(4, 1fr);
+    place-content: center;
+
+    & > .class-item {
+      width: 100%;
+      height: 100%;
+      display: grid;
+      place-content: center;
+    }
+  }
+`;
+
+  html = () => /* HTML */ `
+    <style>
+      ${this.css()}
+    </style>
+    <div class="home">
+      ${["月", "火", "水", "木", "金"]
+        .map((day) =>
+          [0, 1, 2, 3, 4]
+            .map(
+              (period) => /* html */ `
+              <div class="class-item">
+                <span>${day}${period}</span>
+              </div>
+            `
+            )
+            .join("")
+        )
+        .join("")}
+    </div>
+  `;
 
   constructor() {
     super();
